ec241483e59d210c3e911fae276c8227d964c53c
Baizhou Zhang <eddiezhang@pku.edu.cn>
2023-08-17 16:03:04 +0800

    add blip2 pipeline policy

--- colossalai/shardformer/policies/blip2.py
+++ colossalai/shardformer/policies/blip2.py
@@ -1,3 +1,8 @@
+from functools import partial
+from typing import Callable, Dict, List, Tuple
+
+from torch import Tensor, nn
+
 import colossalai.shardformer.layer as col_nn
 
 from ..modeling.blip2 import (
@@ -9,7 +14,7 @@ from ..modeling.blip2 import (
 from ..modeling.jit import get_jit_fused_dropout_add_func
 from .base_policy import ModulePolicyDescription, Policy, SubModuleReplacementDescription
 
-__all__ = ["BlipPolicy", "BlipModelPolicy"]
+__all__ = ['BlipPolicy', 'BlipModelPolicy', 'Blip2ForConditionalGenerationPolicy']
 
 
 class BlipPolicy(Policy):
@@ -21,6 +26,10 @@ class BlipPolicy(Policy):
         r"""
         Reshape the Embedding layer to make the embedding dimension divisible by world_size
         """
+        from transformers import OPTForCausalLM
+        if not isinstance(self.model.language_model, OPTForCausalLM):
+            raise ValueError("Currently Shardformer only supports OPTForCausalLM as language model decoder for Blip2.")
+
         vocab_size = self.model.config.text_config.vocab_size
         world_size = self.shard_config.tensor_parallel_size
         if vocab_size % world_size != 0:
@@ -336,14 +345,209 @@ class BlipPolicy(Policy):
     def postprocess(self):
         return self.model
 
+    @staticmethod
+    def distribute_blip2_layers(num_vision_layers: int, num_qformer_layers: int, num_language_layers: int,
+                                num_stages: int) -> Tuple[List[int], int]:
+        """
+        Distribute blip2 layers into stages when pipeline parallel is used.
+        Return the layer distribution as a list and the starting stage of QFormer model and language model.
+        """
+
+        if num_vision_layers <= 0 or num_qformer_layers <= 0 or num_language_layers <= 0:
+            raise ValueError("The number of layers of vision/qformer/language models should be non-zero 0.")
+
+        # number of layers should be large enough to fill in every stage
+        if num_vision_layers + num_qformer_layers + num_language_layers < num_stages:
+            raise ValueError("The total number of layers can't be smaller than number of stages.")
+
+        # the number of stages distributed between vision, qformer and language model is optmized in this way:
+        # num_vision_stages, num_qformer_stages, num_language_stages = argmin((x - ave) ^ 2 + (y - ave) ^ 2 + (z - ave) ^ 2)
+        # s.t. x = num_vision_layers / num_vision_stages, y = num_qformer_layers / num_qformer_stages, z = num_language_layers / num_language_stages
+        #      ave = (x + y + z) / 3
+        #      num_vision_stages + num_qformer_stages + num_language_stages = num_stages, num_vision_stages >= 1, num_qformer_stages >= 1, num_language_stages >= 1
+
+        def objective(num_vision_stages, num_qformer_stages):
+            x = num_vision_layers / num_vision_stages
+            y = num_qformer_layers / num_qformer_stages
+            z = num_language_layers / (num_stages - num_vision_stages - num_qformer_stages)
+            ave = (x + y + z) / 3
+            return (x - ave)**2 + (y - ave)**2 + (z - ave)**2
+
+        num_vision_stages, num_qformer_stages = 1, 1
+        optimal_diff = 2**31 - 1
+        for i in range(1, num_stages - 1):
+            for j in range(1, num_stages - 1):
+                if (i + j + 1) > num_stages:
+                    break
+            attempt = objective(i, j)
+            if attempt < optimal_diff:
+                num_vision_stages, num_qformer_stages = i, j
+                optimal_diff = attempt
+        num_language_stages = num_stages - num_vision_stages - num_qformer_stages
+
+        vision_distribution = Policy.distribute_layers(num_vision_layers, num_vision_stages)
+        qformer_distribution = Policy.distribute_layers(num_qformer_layers, num_qformer_stages)
+        language_distribution = Policy.distribute_layers(num_language_layers, num_language_stages)
+
+        qformer_starting_stage = num_vision_stages
+        language_starting_stage = num_vision_stages + num_qformer_stages
+
+        return vision_distribution + qformer_distribution + language_distribution, qformer_starting_stage, language_starting_stage
+
+    @staticmethod
+    def get_blip2_stage_index(layers_per_stage: List[int], stage: int, qformer_starting_stage: int,
+                              language_starting_stage: int) -> Tuple[bool, int, int]:
+        """
+        Input the distribution of layers among stages, the current stage and the first stage of qformer/language model.
+        Return the starting/ending idx of layers belonging to this stage.
+        """
+        if stage < qformer_starting_stage:
+            return Policy.get_stage_index(layers_per_stage[:qformer_starting_stage], stage)
+        elif stage < language_starting_stage:
+            return Policy.get_stage_index(layers_per_stage[qformer_starting_stage:language_starting_stage],
+                                          stage - qformer_starting_stage)
+        else:
+            return Policy.get_stage_index(layers_per_stage[language_starting_stage:], stage - language_starting_stage)
+
+    def get_held_layers(self) -> List[nn.Module]:
+        """Get pipeline layers for current stage."""
+        assert self.pipeline_stage_manager is not None
+        stage_manager = self.pipeline_stage_manager
+
+        model = self.model
+        vision_model = self.model.vision_model
+        qformer_model = self.model.qformer
+        language_model = self.model.language_model
+
+        num_vision_layers = len(vision_model.encoder.layers)
+        num_qformer_layers = len(qformer_model.encoder.layer)
+        num_language_layers = len(language_model.model.decoder.layers)
+
+        held_layers = []
+        layers_per_stage, qformer_starting_stage, language_starting_stage = BlipPolicy.distribute_blip2_layers(
+            num_vision_layers, num_qformer_layers, num_language_layers, stage_manager.num_stages)
+        start_idx, end_idx = BlipPolicy.get_blip2_stage_index(layers_per_stage, stage_manager.stage,
+                                                              qformer_starting_stage, language_starting_stage)
+
+        if stage_manager.stage < qformer_starting_stage:
+            # current stage is in blip2's vision model
+            if stage_manager.is_first_stage():
+                held_layers.append(vision_model.embeddings)
+            if stage_manager.stage == qformer_starting_stage - 1:
+                held_layers.append(vision_model.post_layernorm)
+            held_layers.extend(vision_model.encoder.layers[start_idx:end_idx])
+        elif stage_manager.stage < language_starting_stage:
+            # current stage is in blip2's qformer model
+            if stage_manager.stage == qformer_starting_stage:
+                held_layers.append(qformer_model.layernorm)
+                held_layers.append(qformer_model.dropout)
+            if stage_manager.stage == language_starting_stage - 1:
+                # put the projection from qformer output to language input at the last stage of qformer
+                held_layers.append(model.language_projection)
+            held_layers.extend(qformer_model.encoder.layer[start_idx:end_idx])
+        else:
+            # current stage is in blip2's language model
+            if stage_manager.stage == language_starting_stage:
+                held_layers.append(language_model.model.decoder.embed_tokens)
+                held_layers.append(language_model.model.decoder.embed_positions)
+                held_layers.append(language_model.model.decoder.final_layer_norm)
+            if stage_manager.is_last_stage():
+                held_layers.append(language_model.lm_head)
+            held_layers.extend(language_model.model.decoder.layers[start_idx:end_idx])
+        return held_layers
+
+    def set_pipeline_forward(self, model_cls: nn.Module, new_forward: Callable, policy: Dict) -> None:
+        """If under pipeline parallel setting, replacing the original forward method of huggingface
+           to customized forward method, and add this changing to policy."""
+        if not self.pipeline_stage_manager:
+            raise ValueError("set_pipeline_forward method can only be called when pipeline parallel is enabled.")
+        stage_manager = self.pipeline_stage_manager
+
+        num_vision_layers = len(self.model.vision_model.encoder.layers)
+        num_qformer_layers = len(self.model.qformer.encoder.layer)
+        num_language_layers = len(self.model.language_model.model.decoder.layers)
+
+        layers_per_stage, qformer_starting_stage, language_starting_stage = BlipPolicy.distribute_blip2_layers(
+            num_vision_layers, num_qformer_layers, num_language_layers, stage_manager.num_stages)
+        stage_index = BlipPolicy.get_blip2_stage_index(layers_per_stage, stage_manager.stage, qformer_starting_stage,
+                                                       language_starting_stage)
+
+        method_replacement = {
+            'forward':
+                partial(new_forward,
+                        stage_manager=stage_manager,
+                        stage_index=stage_index,
+                        qformer_starting_stage=qformer_starting_stage,
+                        language_starting_stage=language_starting_stage)
+        }
+        self.append_or_create_method_replacement(description=method_replacement, policy=policy, target_key=model_cls)
+
+    def get_shared_params(self) -> List[Dict[int, Tensor]]:
+
+        stage_manager = self.pipeline_stage_manager
+        if stage_manager is not None and stage_manager.num_stages > 1:
+            num_vision_layers = len(self.model.vision_model.encoder.layers)
+            num_qformer_layers = len(self.model.qformer.encoder.layer)
+            num_language_layers = len(self.model.language_model.model.decoder.layers)
+
+            _, _, language_starting_stage = BlipPolicy.distribute_blip2_layers(num_vision_layers, num_qformer_layers,
+                                                                               num_language_layers,
+                                                                               stage_manager.num_stages)
+
+            # embedding and lm_head in language_model should be shared
+            language_embed_tokens_weight = self.model.language_model.model.decoder.embed_tokens.weight
+            lm_head_weight = self.model.language_model.lm_head.weight
+            if id(language_embed_tokens_weight) == id(lm_head_weight):
+                return [{
+                    language_starting_stage: language_embed_tokens_weight,
+                    stage_manager.num_stages - 1: lm_head_weight
+                }]
+
+        return []
+
 
 # Blip2Model
 class Blip2ModelPolicy(BlipPolicy):
     def __init__(self) -> None:
+
         super().__init__()
+
+    def module_policy(self):
+        from transformers import Blip2Model
+        policy = super().module_policy()
+
+        if self.pipeline_stage_manager is not None:
+            self.set_pipeline_forward(model_cls=Blip2Model,
+                                      new_forward=Blip2PipelineForwards.blip2_model_forward,
+                                      policy=policy)
+
+        return policy
+
+    def get_held_layers(self) -> List[nn.Module]:
+        return super().get_held_layers()
+
+    def get_shared_params(self) -> List[Dict[int, Tensor]]:
+        return super().get_shared_params()
 
 
 # Blip2ForConditionalGeneration
 class Blip2ForConditionalGenerationPolicy(BlipPolicy):
     def __init__(self) -> None:
         super().__init__()
+
+    def module_policy(self):
+        from transformers import Blip2ForConditionalGeneration
+        policy = super().module_policy()
+
+        if self.pipeline_stage_manager is not None:
+            self.set_pipeline_forward(model_cls=Blip2ForConditionalGeneration,
+                                      new_forward=Blip2PipelineForwards.blip2_for_conditional_generation_forward,
+                                      policy=policy)
+
+        return policy
+
+    def get_held_layers(self) -> List[nn.Module]:
+        return super().get_held_layers()
+
+    def get_shared_params(self) -> List[Dict[int, Tensor]]:
+        return super().get_shared_params()
